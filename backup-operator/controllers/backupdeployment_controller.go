263ad9b88e2fd0a8101d4434b63e3a00c32c6e9c
citsjtu2020@126.com <3225496803@qq.com>
2022-03-14 18:55:34 +0800

    2022.3.14 update

--- backup-operator/controllers/backupdeployment_controller.go
+++ backup-operator/controllers/backupdeployment_controller.go
@@ -19,12 +19,20 @@ package controllers
 import (
 	"context"
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	//"sigs.k8s.io/controller-runtime/pkg/log"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
+	ref "k8s.io/client-go/tools/reference"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -49,8 +57,24 @@ func (_ realClock) Now() time.Time {
 type Clock interface {
 	Now() time.Time
 }
-
 // +kubebuilder:docs-gen:collapse=Clock
+
+type IndexedDeploy struct {
+	deploy *appsv1.Deployment
+	index int
+}
+// +kubebuilder:docs-gen:collapse=IndexedDeploy
+
+var (
+	ScheduledTimeAnnotation = "elasticscale.com.sjtu.cit/scheduled-at"
+	DeployStateAnnotation = "elasticscale.com.sjtu.cit/state"
+	DeployIDAnnotation = "elasticscale.com.sjtu.cit/id"
+	jobOwnerKey             = ".metadata.controller"
+	apiGVStr                = elasticscalev1.GroupVersion.String()
+	deploy_delete_policy = metav1.DeletePropagationForeground
+)
+
+
 
 // +kubebuilder:rbac:groups=elasticscale.com.sjtu.cit,resources=backupdeployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=elasticscale.com.sjtu.cit,resources=backupdeployments/status,verbs=get;update;patch
@@ -75,6 +99,7 @@ func (r *BackupDeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 	log := r.Log.WithValues("backupdeployment", req.NamespacedName) //logging handle
 
 	var backdeploy elasticscalev1.BackupDeployment
+	
 	if err := r.Get(ctx, req.NamespacedName, &backdeploy); err != nil {
 		log.Error(err, "unable to fetch BackupDeployment")
 		//	// we'll ignore not-found errors, since they can't be fixed by an immediate
@@ -91,11 +116,287 @@ func (r *BackupDeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 		log.V(1).Info("some service are not ready","depend service",fmt.Sprintf("%v",backdeploy.Spec.ServiceName))
 		return ctrl.Result{RequeueAfter: initdur*time.Millisecond}, nil
 	}
+	
+	var childDeploys appsv1.DeploymentList
+	if err := r.List(ctx, &childDeploys, client.InNamespace(req.Namespace), client.MatchingFields{jobOwnerKey: req.Name}); err != nil {
+		log.Error(err, "unable to list child Jobs")
+		return ctrl.Result{}, err
+	}
 
 
+	
+	var activeDeploys map[int]*appsv1.Deployment = make(map[int]*appsv1.Deployment)
+	var backDeploys []*appsv1.Deployment
+	var waitDeploys map[int]*appsv1.Deployment = make(map[int]*appsv1.Deployment)
+	
+	runningreplicas := 0
+	backupreplicas := 0
+
+	for i,deploy := range childDeploys.Items{
+		_, ok2,deployStatus,idx := isDeployStatus(&deploy)
+		switch deployStatus {
+		case string(elasticscalev1.ActiveState):
+			if !ok2{
+				activeDeploys[int(*backdeploy.Status.LastID)] = &childDeploys.Items[i]
+				tmpvalue := *backdeploy.Status.LastID
+				*backdeploy.Status.LastID = (tmpvalue+1)
+			}else{
+				activeDeploys[idx] = &childDeploys.Items[i]
+			}
+			runningreplicas += int(*(&childDeploys.Items[i]).Spec.Replicas)
+		case string(elasticscalev1.WaitingState):
+			if !ok2{
+				waitDeploys[int(*backdeploy.Status.LastID)] = &childDeploys.Items[i]
+				tmpvalue := *backdeploy.Status.LastID
+				*backdeploy.Status.LastID = (tmpvalue+1)
+			}else{
+				waitDeploys[idx] = &childDeploys.Items[i]
+			}
+			runningreplicas += int(*(&childDeploys.Items[i]).Spec.Replicas)
+		case string(elasticscalev1.BackupState):
+			backDeploys = append(backDeploys,&childDeploys.Items[i])
+			backupreplicas += int(*(&childDeploys.Items[i]).Spec.Replicas)
+		case "":
+			continue
+		}
+	}
+	
+	if runningreplicas == int(*backdeploy.Spec.RunningReplicas) && backupreplicas == int(*backdeploy.Spec.BackupReplicas){
+		
+	}else{
+		if runningreplicas < int(*backdeploy.Spec.RunningReplicas){
+			
+		}else{
+			if runningreplicas > int(*backdeploy.Spec.RunningReplicas){
+				aim_scale_in := runningreplicas - int(*backdeploy.Spec.RunningReplicas)
+				now_scale := 0
+				deletes := make(map[string][]*IndexedDeploy)
+				deletes["wait"] = []*IndexedDeploy{}
+				deletes["active"] = []*IndexedDeploy{}
+				scalein := make(map[string]map[int][]*IndexedDeploy)
+				scalein["wait"] = make(map[int][]*IndexedDeploy)
+				scalein["active"] = make(map[int][]*IndexedDeploy)
+				if len(waitDeploys) > 0{
+					waitid := []int{}
+					for k,_ := range waitDeploys{
+						waitid = append(waitid,k)
+					}
+					sort.Ints(waitid)
+					for j:=len(waitid)-1;j>=0;j--{
+						tmp_rep := int(*waitDeploys[waitid[j]].Spec.Replicas)
+						if now_scale + tmp_rep <= aim_scale_in{
+							select_wait := new(IndexedDeploy)
+							select_wait.deploy = waitDeploys[waitid[j]]
+							select_wait.index = waitid[j]
+							deletes["wait"] = append(deletes["wait"],select_wait)
+							now_scale = now_scale+tmp_rep
+						}else{
+							if now_scale >= aim_scale_in{
+								break
+							}
+							tmp_scale_in,_ := scalein["wait"][int(aim_scale_in - now_scale)]
+							select_wait := new(IndexedDeploy)
+							select_wait.deploy = waitDeploys[waitid[j]]
+							select_wait.index = waitid[j]
+							tmp_scale_in = append(tmp_scale_in,select_wait)
+							scalein["wait"][int(aim_scale_in - now_scale)] = tmp_scale_in
+							now_scale = aim_scale_in
+							break
+						}
+					}
+				}
+				if now_scale < aim_scale_in{
+					if len(activeDeploys) > 0{
+						activeid := []int{}
+						for k,_ := range activeDeploys{
+							activeid = append(activeid,k)
+						}
+						sort.Ints(activeid)
+						for j:=len(activeid)-1;j>=0;j--{
+							tmp_rep := int(*activeDeploys[activeid[j]].Spec.Replicas)
+							if now_scale + tmp_rep <= aim_scale_in{
+								select_active := new(IndexedDeploy)
+								select_active.deploy = waitDeploys[activeid[j]]
+								select_active.index = activeid[j]
+								deletes["active"] = append(deletes["active"],select_active)
+								now_scale = now_scale+tmp_rep
+							}else{
+								if now_scale >= aim_scale_in{
+									break
+								}
+								tmp_scale_in,_ := scalein["active"][int(aim_scale_in - now_scale)]
+								select_active := new(IndexedDeploy)
+								select_active.deploy = waitDeploys[activeid[j]]
+								select_active.index = activeid[j]
+								tmp_scale_in = append(tmp_scale_in,select_active)
+								scalein["active"][int(aim_scale_in - now_scale)] = tmp_scale_in
+								now_scale = aim_scale_in
+								break
+							}
+						}
+					}
+				}
+				if len(deletes["wait"])>0{
+					for _,delete2 := range deletes["wait"]{
+						if err := r.Delete(ctx, delete2.deploy, client.PropagationPolicy(deploy_delete_policy)); client.IgnoreNotFound(err) != nil {
+							log.Error(err, "unable to delete the deployment", "deployment", delete2)
+							//delete(waitDeploys,delete2.index)
+							continue
+						} else {
+							log.V(0).Info("deleted the deployment", "deployment", delete2)
+							delete(waitDeploys,delete2.index)
+						}
+					}
+				}
+
+				if len(deletes["active"])>0{
+					for _,delete2 := range deletes["active"]{
+						if err := r.Delete(ctx, delete2.deploy, client.PropagationPolicy(deploy_delete_policy)); client.IgnoreNotFound(err) != nil {
+							log.Error(err, "unable to delete the deployment", "deployment", delete2)
+							//delete(activeDeploys,delete2.index)
+							continue
+						} else {
+							log.V(0).Info("deleted the deployment", "deployment", delete2)
+							delete(activeDeploys,delete2.index)
+						}
+					}
+				}
+				
+				for to_in,deploys2 := range scalein["wait"]{
+					for _,deploy2 := range deploys2{
+						if int32(to_in) >= *(deploy2.deploy.Spec.Replicas){
+							err := r.DeleteDeploy(ctx,deploy2.deploy)
+							if err != nil{
+								log.Error(err, "unable to delete the deployment", "deployment", deploy2)
+								//delete(waitDeploys,deploy2.index)
+								continue
+							}
+							delete(waitDeploys,deploy2.index)
+						}else{
+							err := r.Scalein(ctx,deploy2.deploy,to_in)
+							if err != nil{
+								log.Error(err, "unable to scale in the deployment", "deployment", deploy2)
+								continue
+							}
+						}
+					}
+				}
+
+				for to_in,deploys2 := range scalein["active"]{
+					for _,deploy2 := range deploys2{
+						if int32(to_in) >= *(deploy2.deploy.Spec.Replicas){
+							err := r.DeleteDeploy(ctx,deploy2.deploy)
+							if err != nil{
+								log.Error(err, "unable to delete the deployment", "deployment", deploy2)
+								//delete(activeDeploys,deploy2.index)
+								continue
+							}
+							delete(activeDeploys,deploy2.index)
+						}else{
+							err := r.Scalein(ctx,deploy2.deploy,to_in)
+							if err != nil{
+								log.Error(err, "unable to scale in the deployment", "deployment", deploy2)
+								continue
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		if backupreplicas != int(*backdeploy.Spec.BackupReplicas){
+
+		}
+	}
+	
+	for _, activeDeploy := range activeDeploys {
+		deployRef, err := ref.GetReference(r.Scheme, activeDeploy)
+		if err != nil {
+			log.Error(err, "unable to make reference to the running deployment", "deployment", activeDeploy)
+			continue
+		}
+		backdeploy.Status.Active = append(backdeploy.Status.Active, *deployRef)
+	}
+	
+	for _, backDeploy := range backDeploys {
+		deployRef, err := ref.GetReference(r.Scheme, backDeploy)
+		if err != nil {
+			log.Error(err, "unable to make reference to the running deployment", "deployment", backDeploy)
+			continue
+		}
+		backdeploy.Status.Back = append(backdeploy.Status.Back, *deployRef)
+	}
+	
+	for _, waitDeploy := range waitDeploys {
+		deployRef, err := ref.GetReference(r.Scheme, waitDeploy)
+		if err != nil {
+			log.Error(err, "unable to make reference to the running deployment", "deployment", waitDeploy)
+			continue
+		}
+		backdeploy.Status.Wait = append(backdeploy.Status.Wait, *deployRef)
+	}
+	
+	
+	
+	//for i,deploy
 
 	return ctrl.Result{}, nil
 }
+
+//func getIndexedDeploy (deploys []*appsv1.Deployment)
+// apierrors.IsNotFound(err)
+
+func (r *BackupDeploymentReconciler)DeleteDeploy(ctx context.Context,deploy *appsv1.Deployment) error{
+	if err := r.Delete(ctx, deploy, client.PropagationPolicy(deploy_delete_policy)); client.IgnoreNotFound(err) != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+// +kubebuilder:docs-gen:collapse=DeleteDeploy
+
+func (r *BackupDeploymentReconciler)Scalein(ctx context.Context,deploy *appsv1.Deployment,to_in int)  error {
+	tmp_rep := *(deploy.Spec.Replicas)
+	*(deploy.Spec.Replicas) = int32(tmp_rep - int32(to_in))
+	err := r.Update(ctx,deploy)
+	if err != nil{
+		if apierrors.IsNotFound(err){
+			err = r.Create(ctx,deploy)
+		}
+		if client.IgnoreNotFound(err) != nil{
+			return err
+		}
+	}
+	return nil
+}
+// +kubebuilder:docs-gen:collapse=Scalein
+
+func isDeployStatus(deploy *appsv1.Deployment) (bool,bool,string,int) {
+		//for k, c := range deploy.Annotations {
+		//	//if (c.Type == kbatch.JobComplete || c.Type == kbatch.JobFailed) && c.Status == corev1.ConditionTrue {
+		//	//	return true, c.Type
+		//	//}
+		//	if strings.EqualFold(k,DeployStateAnnotation){
+		//		if strings.EqualFold(c,string(elasticscalev1.WaitingState)) || strings.EqualFold(c,string(elasticscalev1.ActiveState)) || strings.EqualFold(c,string(elasticscalev1.BackupState)){
+		//			return true,c
+		//		}
+		//	}
+		//}
+		 v1,_ := deploy.Annotations[DeployStateAnnotation]
+		 v2,ok2 := deploy.Annotations[DeployIDAnnotation]
+		 id := -1
+		 if !ok2{
+		 	id = -1
+		 }else{
+		 	id,_ =strconv.Atoi(v2)
+		 }
+
+		 if strings.EqualFold(v1,string(elasticscalev1.WaitingState)) || strings.EqualFold(v1,string(elasticscalev1.ActiveState)) || strings.EqualFold(v1,string(elasticscalev1.BackupState)){
+		 	return true,ok2,v1,id
+		 }
+		return false,false, "",0
+	}
+	// +kubebuilder:docs-gen:collapse=isDeployStatus
 
 func (r *BackupDeploymentReconciler) findLabels(ctx context.Context,bd elasticscalev1.BackupDeployment)(labels map[string]string){
 	services := bd.Spec.ServiceName
@@ -116,6 +417,7 @@ func (r *BackupDeploymentReconciler) findLabels(ctx context.Context,bd elasticsc
 	}
 	return
 }
+// +kubebuilder:docs-gen:collapse=findLabels
 
 func (r *BackupDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
